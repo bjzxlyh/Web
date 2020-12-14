da80cfdd6521838ec0d540146b7639224c101112
liyiheng <834805011@qq.com>
2020-12-14 21:08:40 +0800

    secondweb

---
+++ src/main/java/serverlet/Mysecondserverlet.java
@@ -0,0 +1,17 @@
+package serverlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Ser01")
+public class Mysecondserverlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet被访问了");
+
+    }
+}
